e63a64aadb5c4a1341119a5a38d914fb593eaf30
EnCglds <124785557+EnCglds@users.noreply.github.com>
2024-02-22 12:30:27 -0300

    Create Prova.js

---
+++ Prova.js
@@ -0,0 +1,35 @@
+Nome: Enzo Corbanezi Gasques Landim De Souza 
+Nome: Pedro Gustavo De Godoi
+
+
+let aluno = {
+    nome: "Enzo Corbanezi Gasques Landim De Souza", 
+    idade: 16,
+    cursando: ["PDM","AOS,","IA"],
+    presença: ["2024-02-22", "2024-02-21"],
+    ra: "50896789" 
+}
+
+
+{ 
+    status: "valid",
+    message: "Cadastro válido",
+    aluno: {
+   if(cursando == 3){
+    console.log("cadstro valido")
+   }
+    }
+ if(presença == 2){
+    console.log("cadstro valido")
+   }
+}
+
+{ 
+console.log("Erro: O nome deve conter pelo menos  10 caracteres")
+console.log("Erro: A idade do aulo de ser superior a 14 anos") 
+console.log("Erro: O aluno deve estar cursando 3 matérias") 
+console.log("Erro: O aluno deve ter no minimo 3 presenças") 
+console.log("Erro: O RA do aluno deve ter 7 caracteres") 
+    return; 
+}
+
